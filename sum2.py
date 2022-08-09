96a49853f6f1631f0424a98e25163a017a79998b
Raseent <110896636+Raseent@users.noreply.github.com>
2022-08-09 15:03:37 +0530

    First commit

---
+++ sum2.py
@@ -0,0 +1,16 @@
+mul=1
+for i in range(1,11):
+    mul=mul*i
+print(mul)
+print("=======================================")
+num=12345
+sum=0
+for i in range(len(str(num))):
+    temp=num%10
+    num=num//10
+    print(num)
+    sum+=temp
+print(sum)
+print("======================================")
+#
+
