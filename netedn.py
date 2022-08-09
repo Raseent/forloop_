02d9654ca1f2ac37bfad6b1e89f9da91991c8a56
Raseent <110896636+Raseent@users.noreply.github.com>
2022-08-09 15:21:22 +0530

    dic

---
+++ netedn.py
@@ -0,0 +1,23 @@
+nested_dictionary={
+         "one":
+         {
+             "name":"john",
+             "age":30,
+             "city":"new york"
+         },
+         "two":
+{
+
+         "name":"jane",
+         "age":25,
+         "city":"paris"
+},
+        "three":
+{
+        "name":"bobby",
+        "age":20,
+        "city":"london"
+}
+}
+print(nested_dictionary)
+print(nested_dictionary["three"]["name"])
